92b21eefa7452e1f583453891c1efe5943485754
Marco Roganovic <marcoroganovic@gmail.com>
2016-12-13 11:42:14 +0100

    Move server creation to index.js and clean up router.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-var Router     = require("./lib/router.js");
-module.exports = Router;
+var http        = require("http"),
+    request     = require("./lib/request"),
+    response    = require("./lib/response"),
+    findHandler = require("./lib/router").findHandler,
+    Router      = require("./lib/router").Router,
+    notFound    = require("./lib/not-found");
+
+var server = http.createServer((req, res) => {
+  request(req);
+  response(res);
+
+  var handler = findHandler(req);
+
+  if(handler) {
+    handler(req, res);
+  } else {
+    notFound(req, res);
+  }
+});
+
+function listen(port) {
+  server.listen(port, () => console.log("Server running on port " + port));
+}
+
+module.exports = function() {
+  var router = Router();
+  router.listen = listen;
+  return router;
+}
